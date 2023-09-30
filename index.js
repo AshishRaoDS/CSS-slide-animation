f95a97ad8b6f05588ebfe2df1bfca91175bad60f
Ashish Rao D S <ashishraods@SMGCC-MAC014.local>
2023-09-30 21:42:16 +0530

    clean up and trying a fix for mobile

--- index.js
+++ index.js
@@ -3,37 +3,50 @@ let position = 0
 imageWrapper.dataset.prevPosition = "0"
 imageWrapper.dataset.currentPosition = '0'
 
-window.onmousedown = function(e) {
+function onClick(e) {
     var x = e.clientX;
     var y = e.clientY;
     imageWrapper.dataset.currentPosition = e.clientX
  
 }
 
-window.onmousemove = function(e) {
+function handleSlide(e) {
     const currentPosition = imageWrapper.dataset.currentPosition;
     const prevPosition = imageWrapper.dataset.prevPosition;
     if(currentPosition == 0) return;
 
     const movePercentage = ((((parseFloat(e.clientX) - parseFloat(currentPosition)) / +window.innerWidth) * -100) + parseFloat(prevPosition))
     const finalPercentage =  Math.max(Math.min(movePercentage, 0), -100) || 0
-    // const finalPercentage = movePercentage
     imageWrapper.dataset.percentage = finalPercentage
     imageWrapper.animate({
         transform: `translate(${finalPercentage}%, -50%)`
     }, {duration:1200, fill: "forwards"})
-    // const translateValue = `translate(${finalPercentage}%, -50%)`
-    console.log(finalPercentage)
-    // imageWrapper.style.transform = translateValue
 
     for(const image of imageWrapper.getElementsByClassName("image")) {
         image.animate({
           objectPosition: `${100 + finalPercentage}% center`
         }, { duration: 1200, fill: "forwards" });
-      }
+    }
 }
 
-window.onmouseup = function(e) {
+ function onRelease(e) {
     imageWrapper.dataset.currentPosition = 0 ;
     imageWrapper.dataset.prevPosition = imageWrapper.dataset.percentage;
 }
+
+window.onmousemove = handleSlide
+
+window.onmousedown = onClick
+
+window.onmouseup = onRelease
+
+
+window.ontouchstart = e => onClick(e.touches[0]);
+
+// window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => onRelease(e.touches[0]);
+
+// window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleSlide(e.touches[0]);
